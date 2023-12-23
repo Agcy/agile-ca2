943b130adfc14dae2d5ead7927edb9e641753490
Haopeng Liang <765407782@qq.com>
2023-12-23 15:16:46 +0000

    Changes due to API from WAD2 lab

--- index.js
+++ index.js
@@ -1,24 +1,38 @@
-import express from 'express';
 import dotenv from 'dotenv';
-import cors from 'cors';
-import usersRouter from './api/users';
-import moviesRouter from './api/movies';   //import movies router
-import authenticate from './authenticate';
+import express from 'express';
+import moviesRouter from './api/movies';
+// import genresRouter from './api/genres'
 import './db';
-import defaultErrHandler from './errHandler'
 import './seedData'
+import usersRouter from './api/users';
+// import authenticate from './authenticate';
+import passport from './authenticate';
+import './seedData'
+
+const errHandler = (err, req, res, next) => {
+    /* if the error in development then send stack trace to display whole error,
+    if it's in production then just send error message  */
+    if(process.env.NODE_ENV === 'production') {
+        return res.status(500).send(`Something went wrong!`);
+    }
+    res.status(500).send(`Hey!! You caught the error 👍👍. Here's the details: ${err.stack} `);
+};
 
 dotenv.config();
 
 const app = express();
+
 const port = process.env.PORT;
 
-app.use(cors());
+app.use(passport.initialize());
 app.use(express.json());
+// app.use('/api/movies', passport.authenticate('jwt', {session: false}), moviesRouter);
+app.use('/api/movies', moviesRouter);
+// app.use('/api/genres', genresRouter);
 app.use('/api/users', usersRouter);
-app.use(defaultErrHandler);
-app.use('/api/movies',authenticate,  moviesRouter);
+app.use(errHandler);
 
-app.listen(port, () => {
+let server = app.listen(port, () => {
     console.info(`Server running at ${port}`);
 });
+module.exports = server
