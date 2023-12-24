5c96c3dd97e5bf181e48a09ba4320c4e3994a4fb
Haopeng Liang <765407782@qq.com>
2023-12-24 20:02:31 +0000

    refactor authenticate complete

--- api/users/index.js
+++ api/users/index.js
@@ -2,28 +2,28 @@ import express from 'express';
 import User from './userModel';
 import asyncHandler from "express-async-handler";
 import jwt from 'jsonwebtoken';
-
+import {authenticate} from "../../authenticate";
 const router = express.Router(); // eslint-disable-line
 
 // Middleware for authentication
-const authenticate = async (req, res, next) => {
-    const token = req.headers.authorization?.split(' ')[1]; // Bearer <token>
-    if (!token) {
-        return res.status(401).json({ message: 'No token provided.' });
-    }
-
-    try {
-        const decoded = jwt.verify(token, process.env.SECRET);
-
-        console.info(decoded)
-
-        const user = await User.findByUserName(decoded.username)
-        req.user = user;
-        next();
-    } catch (error) {
-        res.status(401).json({ message: 'Invalid token.' });
-    }
-};
+// const authenticate = async (req, res, next) => {
+//     const token = req.headers.authorization?.split(' ')[1]; // Bearer <token>
+//     if (!token) {
+//         return res.status(401).json({ message: 'No token provided.' });
+//     }
+//
+//     try {
+//         const decoded = jwt.verify(token, process.env.SECRET);
+//
+//         console.info(decoded)
+//
+//         const user = await User.findByUserName(decoded.username)
+//         req.user = user;
+//         next();
+//     } catch (error) {
+//         res.status(401).json({ message: 'Invalid token.' });
+//     }
+// };
 
 // Get all users
 router.get('/', async (req, res) => {
--- authenticate/index.js
+++ authenticate/index.js
@@ -2,23 +2,45 @@ import passport from 'passport';
 import passportJWT from 'passport-jwt';
 import UserModel from './../api/users/userModel';
 import dotenv from 'dotenv';
+import jwt from "jsonwebtoken";
+import User from "../api/users/userModel";
 
 dotenv.config();
 
-const JWTStrategy = passportJWT.Strategy;
-const ExtractJWT = passportJWT.ExtractJwt;
+// const JWTStrategy = passportJWT.Strategy;
+// const ExtractJWT = passportJWT.ExtractJwt;
+//
+// let jwtOptions = {};
+// jwtOptions.jwtFromRequest = ExtractJWT.fromAuthHeaderAsBearerToken();
+// jwtOptions.secretOrKey = process.env.SECRET;
+// const strategy = new JWTStrategy(jwtOptions, async (payload, next) => {
+//     const user = await UserModel.findByUserName(payload);
+//     if (user) {
+//         next(null, user);
+//     } else {
+//         next(null, false);
+//     }
+// });
+// passport.use(strategy);
 
-let jwtOptions = {};
-jwtOptions.jwtFromRequest = ExtractJWT.fromAuthHeaderAsBearerToken();
-jwtOptions.secretOrKey = process.env.SECRET;
-const strategy = new JWTStrategy(jwtOptions, async (payload, next) => {
-    const user = await UserModel.findByUserName(payload);
-    if (user) {
-        next(null, user);
-    } else {
-        next(null, false);
+// 自定义身份验证中间件
+const authenticate = async (req, res, next) => {
+    const token = req.headers.authorization?.split(' ')[1]; // Bearer <token>
+    if (!token) {
+        return res.status(401).json({ message: 'No token provided.' });
     }
-});
-passport.use(strategy);
 
-export default passport;
+    try {
+        const decoded = jwt.verify(token, process.env.SECRET);
+
+        console.info(decoded)
+
+        const user = await User.findByUserName(decoded.username)
+        req.user = user;
+        next();
+    } catch (error) {
+        res.status(401).json({ message: 'Invalid token.' });
+    }
+};
+
+export { authenticate };
